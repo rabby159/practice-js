153a265846cd3ad970436ff4f08fb696f5af4ef2
Md Rabby <ablzerabbi159@gmail.com>
2023-08-06 14:46:29 +0600

    feet to inches fine

---
+++ practice24.js
@@ -0,0 +1,10 @@
+// finding feet to inches
+
+function feetToInch(value){
+    const calculation = value * 12; //1 feet = 12inch
+
+    return calculation;
+};
+
+const feetValue = feetToInch(35);
+console.log("Inches value is: ",feetValue);
